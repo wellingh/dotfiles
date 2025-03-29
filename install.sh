50f8472d4b49a9142f8dc27918cd5d7e81968ea0
wellingh <wmaramos@gmail.com>
2025-03-29 10:24:30 +0100

    enhance darwin install

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
-if command -v apt >/dev/null 2>&1; then
-    sudo apt update
-    sudo apt install build-essential \
-        stow curl silversearcher-ag wcstools -y
+OS="$(uname -s)"
 
-    sudo locale-gen en_US.UTF-8
-    sudo update-locale LANG=en_US.UTF-8
+if [[ $OS == "Linux" ]]; then
+    if command -v apt >/dev/null 2>&1; then
+        sudo apt update
+        sudo apt install build-essential \
+            stow curl silversearcher-ag wcstools -y
 
+        sudo locale-gen en_US.UTF-8
+        sudo update-locale LANG=en_US.UTF-8
 
-elif command -v dnf >/dev/null 2>&1; then
-    echo "This is a Red Hat (or RPM-based) system."
+    elif command -v dnf >/dev/null 2>&1; then
+        echo "This is a Red Hat (or RPM-based) system."
 
-else
-    echo "Unknown package manager. Cannot determine the distribution."
-    exit 1
+    else
+        echo "Unknown package manager. Cannot determine the distribution."
+        exit 1
+    fi
 fi
 
 if ! command -v brew >/dev/null 2>&1; then
@@ -25,20 +28,22 @@ DOTFILES="$(dirname "${BASH_SOURCE[0]}")"
 
 # STOW_IGNORE=$(paste -sd '|' "$DOTFILES/.stow-local-ignore")
 
-mkdir -p ~/.bkp_dotfiles/
+DOTFILES_BKP="$HOME/.bkp_dotfiles"
+
+mkdir -p "$DOTFILES_BKP"
 
 for item in {$DOTFILES/*,$DOTFILES/.*}; do
     # Skip items in the ignore list
     # shellcheck disable=SC2199
     # shellcheck disable=SC2076
-    if ! grep -qE "^$(filename $item)$" .stow-local-ignore ; then
+    if ! grep -qE "^$(basename $item)$" .stow-local-ignore ; then
         # Process the item (file or directory)
         if [[ -f "$HOME/$item" ]] && [[ -L "$HOME/$item" ]]; then
             continue
         else
             if [[ -f "$HOME/$item" ]]; then
                 echo "Moving $HOME/$item to backup ~/.dotfiles"
-                echo mv "$HOME/$item" ~/.dotfiles/
+                mv "$HOME/$item" "$DOTFILES_BKP"
             fi
         fi
     fi
@@ -47,13 +52,13 @@ done
 stow "$DOTFILES" -t "$HOME"
 
 # bash-it completions
-$SHELL -i -c "bash-it enable completion brew"
+$SHELL -i -c "bash-it enable completion defaults brew docker gcloud git kubectl"
 
 # bash-it plugins
-$SHELL -i -c "bash-it enable plugins gitstatus git-subrepo"
+$SHELL -i -c "bash-it enable plugins gitstatus git-subrepo pyenv"
 
 # install homebrew packages
-for pkg in tfenv terraform-docs terraform-ls uv; do
+for pkg in tfenv terraform-docs terraform-ls uv gh pyenv; do
     command -v $pkg 2>&1 || $SHELL -i -c "brew install $pkg"
 done
 
